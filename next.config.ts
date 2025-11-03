f349d16b73480040c4c9c1d435612c02a079c4c6
Sujal Singhal <tinsusinghal@gmail.com>
2025-11-03 18:16:25 +0530

    1. Fixes the bug of form submitting in deployed site 2. updated the next.config.ts for the fixing it

--- next.config.ts
+++ next.config.ts
@@ -8,6 +8,11 @@ const nextConfig: NextConfig = {
   images: {
     unoptimized: true,
   },
+  env: {
+    NEXT_PUBLIC_SERVICE_ID: process.env.NEXT_PUBLIC_SERVICE_ID,
+    NEXT_PUBLIC_TEMPLATE_ID: process.env.NEXT_PUBLIC_TEMPLATE_ID,
+    NEXT_PUBLIC_PUBLIC_KEY: process.env.NEXT_PUBLIC_PUBLIC_KEY,
+  },
 };
 
 export default nextConfig;
